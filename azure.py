6e83716fd64fd5795e1c26858d105139276642a5
Magesh Anand <Magesh.Anand@asg.com>
2019-11-11 16:15:24 +0530

    add azure file

---
+++ azure.py
@@ -0,0 +1,11 @@
+from pyspark.sql import SparkSession
+
+def jdbc_oracle_example1(spark):
+
+       oracleDF = spark.read.jdbc("jdbc:sqlserver://esbcnprodcap02v.asg.com:1433;databaseName=pubs", "dbo.titles",properties={"user": "sa", "password": "qwer1234$","driver": "com.microsoft.sqlserver.jdbc.SQLServerDriver"})
+       oracleDF.write.jdbc("jdbc:sqlserver://esbcnprodcap02v.asg.com:1433;databaseName=pubs", "dbo.business_tag",properties={"user": "sa", "password": "qwer1234$","driver": "com.microsoft.sqlserver.jdbc.SQLServerDriver"})
+
+if __name__ == "__main__":
+       spark = SparkSession.builder.appName("jdbc_api").getOrCreate()
+       jdbc_oracle_example1(spark)
+       spark.stop()
