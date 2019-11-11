cc9bc80cdd3437635e5e2dbf51094853286e2a47
MageshAnand <magesh.anand6@gmail.com>
2019-11-11 15:35:57 +0530

    Update redshift_format.py

--- redshift_format.py
+++ redshift_format.py
@@ -4,7 +4,7 @@ def jdbc_redshift_example(spark):
     # Reading from RedShift source
     redshiftDF = spark.read \
         .format("com.databricks.spark.redshift") \
-        .option("url", "jdbc:redshift://aws-redshift.c3iskdv9vipb.us-east-1.redshift.amazonaws.com:5439/world?user=master&password=test123") \
+        .option("url", "jdbc:redshift://aws-redshift.csvp5wcqqxvw.us-east-1.redshift.amazonaws.com:5439/world?user=master&password=test123") \
         .option("dbtable", "demo.city") \
 		.option("tempdir", "s3n://path/for/temp/data") \
         .load()
@@ -12,7 +12,7 @@ def jdbc_redshift_example(spark):
     # Saving data to a RedShift source
     redshiftDF.write \
         .format("com.databricks.spark.redshift") \
-        .option("url", "jdbc:redshift://aws-redshift.c3iskdv9vipb.us-east-1.redshift.amazonaws.com:5439/world?user=master&password=test123") \
+        .option("url", "jdbc:redshift://aws-redshift.csvp5wcqqxvw.us-east-1.redshift.amazonaws.com:5439/world?user=master&password=test123") \
         .option("dbtable", "demo.city1") \
         .option("tempdir", "s3n://path/for/temp/data") \
 		.mode("error") \
