701c8ee5b39af5b6e2d1ccb4db1dba8229b75edd
MageshAnand <magesh.anand6@gmail.com>
2019-11-12 13:47:24 +0530

    Update azure.py

--- azure.py
+++ azure.py
@@ -1,6 +1,6 @@
 from pyspark.sql import SparkSession
 
-def jdbc_oracle_example1(spark):
+def jdbc_oracle_example1111(spark):
 
        oracleDF = spark.read.jdbc("jdbc:sqlserver://esbcnprodcap02v.asg.com:1433;databaseName=pubs", "dbo.titles",properties={"user": "sa", "password": "qwer1234$","driver": "com.microsoft.sqlserver.jdbc.SQLServerDriver"})
        oracleDF.write.jdbc("jdbc:sqlserver://esbcnprodcap02v.asg.com:1433;databaseName=pubs", "dbo.business_tag",properties={"user": "sa", "password": "qwer1234$","driver": "com.microsoft.sqlserver.jdbc.SQLServerDriver"})
