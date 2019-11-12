c15297ec31f83ef9c60f301b4bf4a371648a401e
MageshAnand <magesh.anand6@gmail.com>
2019-11-12 13:52:57 +0530

    Update azure.py

--- azure.py
+++ azure.py
@@ -2,8 +2,8 @@ from pyspark.sql import SparkSession
 
 def jdbc_oracle_example1111(spark):
 
-       oracleDF = spark.read.jdbc("jdbc:sqlserver://esbcnprodcap02v.asg.com:1433;databaseName=pubs", "dbo.titles",properties={"user": "sa", "password": "qwer1234$","driver": "com.microsoft.sqlserver.jdbc.SQLServerDriver"})
-       oracleDF.write.jdbc("jdbc:sqlserver://esbcnprodcap02v.asg.com:1433;databaseName=pubs", "dbo.business_tag",properties={"user": "sa", "password": "qwer1234$","driver": "com.microsoft.sqlserver.jdbc.SQLServerDriver"})
+       oracleDF1 = spark.read.jdbc("jdbc:sqlserver://esbcnprodcap02v.asg.com:1433;databaseName=pubs", "dbo.titles",properties={"user": "sa", "password": "qwer1234$","driver": "com.microsoft.sqlserver.jdbc.SQLServerDriver"})
+       oracleDF1.write.jdbc("jdbc:sqlserver://esbcnprodcap02v.asg.com:1433;databaseName=pubs", "dbo.business_tag",properties={"user": "sa", "password": "qwer1234$","driver": "com.microsoft.sqlserver.jdbc.SQLServerDriver"})
 
 if __name__ == "__main__":
        spark = SparkSession.builder.appName("jdbc_api").getOrCreate()
