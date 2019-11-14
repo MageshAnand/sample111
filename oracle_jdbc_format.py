7cbef60b5f6821e9ee6b270e406a70235f2bf610
MageshAnand <magesh.anand6@gmail.com>
2019-11-14 15:58:33 +0530

    intersectAll

--- oracle_jdbc_format.py
+++ oracle_jdbc_format.py
@@ -3,11 +3,10 @@ from __future__ import print_function
 from pyspark.sql import SparkSession
 from pyspark.sql import Row
 
-
 def jdbc_dataset_example(spark):
-    peopleDF = spark.read.format("jdbc").option("url", "jdbc:oracle:thin:@localhost:1521:orcl").option("driver","oracle.jdbc.driver.OracleDriver").option("dbtable", "COLLECTOR.PEOPLE").option("user", "pyspark").option("password", "pyspark").load()
-    adultsDF = spark.read.format("jdbc").option("url", "jdbc:oracle:thin:@localhost:1521:orcl").option("driver","oracle.jdbc.driver.OracleDriver").option("dbtable", "COLLECTOR.ADULTS").option("user", "pyspark").option("password", "pyspark").load()
-    jdbcDF = peopleDF.intersect(adultsDF)
+    df1 = spark.read.format("jdbc").option("url", "jdbc:oracle:thin:@localhost:1521:orcl").option("driver","oracle.jdbc.driver.OracleDriver").option("dbtable", "COLLECTOR.PEOPLE").option("user", "pyspark").option("password", "pyspark").load()
+    df2 = spark.read.format("jdbc").option("url", "jdbc:oracle:thin:@localhost:1521:orcl").option("driver","oracle.jdbc.driver.OracleDriver").option("dbtable", "COLLECTOR.ADULTS").option("user", "pyspark").option("password", "pyspark").load()
+    jdbcDF = df1.intersectAll(df2)
     
 if __name__ == "__main__":
     spark = SparkSession.builder.appName("Python Spark SQL data source example").getOrCreate()
