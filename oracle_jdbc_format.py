c19a7f9cf32f7d70ea214e694df66312974e7a02
MageshAnand <magesh.anand6@gmail.com>
2019-11-15 15:10:30 +0530

    Update oracle_jdbc_format.py

--- oracle_jdbc_format.py
+++ oracle_jdbc_format.py
@@ -4,8 +4,8 @@ from pyspark.sql import Row
  
 def jdbc_subtract_example(spark):
     dep1DF = spark.read.format("jdbc").option("url", "jdbc:oracle:thin:@localhost:1521:orcl").option("driver","oracle.jdbc.driver.OracleDriver").option("dbtable", "COLLECTOR.DEPARTMENT1").option("user", "pyspark").option("password", "pyspark").load()
-    CUSTDF = spark.read.format("jdbc").option("url", "jdbc:oracle:thin:@localhost:1521:orcl").option("driver","oracle.jdbc.driver.OracleDriver").option("dbtable", "COLLECTOR.CUSTOMERS").option("user", "pyspark").option("password", "pyspark").load()
-    departmentDF = CUSTDF.subtract(dep1DF)
+    custDF = spark.read.format("jdbc").option("url", "jdbc:oracle:thin:@localhost:1521:orcl").option("driver","oracle.jdbc.driver.OracleDriver").option("dbtable", "COLLECTOR.CUSTOMERS").option("user", "pyspark").option("password", "pyspark").load()
+    departmentDF = custDF.subtract(dep1DF)
     
 if __name__ == "__main__":
     spark = SparkSession.builder.appName("Python Spark SQL data source example").getOrCreate()
