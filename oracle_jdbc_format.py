cf6ea7e1b4cff7e1070697b24f8d73c0fdfb2c40
MageshAnand <magesh.anand6@gmail.com>
2019-11-15 13:31:40 +0530

    subtract

--- oracle_jdbc_format.py
+++ oracle_jdbc_format.py
@@ -2,12 +2,12 @@ from __future__ import print_function
 from pyspark.sql import SparkSession
 from pyspark.sql import Row
  
-def jdbc_join_example(spark):
+def jdbc_subtract_example(spark):
     dep1DF = spark.read.format("jdbc").option("url", "jdbc:oracle:thin:@localhost:1521:orcl").option("driver","oracle.jdbc.driver.OracleDriver").option("dbtable", "COLLECTOR.DEPARTMENT1").option("user", "pyspark").option("password", "pyspark").load()
     dep2DF = spark.read.format("jdbc").option("url", "jdbc:oracle:thin:@localhost:1521:orcl").option("driver","oracle.jdbc.driver.OracleDriver").option("dbtable", "COLLECTOR.DEPARTMENT2").option("user", "pyspark").option("password", "pyspark").load()
     departmentDF = dep2DF.subtract(dep1DF)
     
 if __name__ == "__main__":
     spark = SparkSession.builder.appName("Python Spark SQL data source example").getOrCreate()
-    jdbc_join_example(spark)
+    jdbc_subtract_example(spark)
     spark.stop()
