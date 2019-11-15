86626252ef9bdc13492a7412c35332deb3f22d54
MageshAnand <magesh.anand6@gmail.com>
2019-11-15 15:17:10 +0530

    Update oracle_jdbc_format.py

--- oracle_jdbc_format.py
+++ oracle_jdbc_format.py
@@ -3,9 +3,9 @@ from pyspark.sql import SparkSession
 from pyspark.sql import Row
  
 def jdbc_subtract_example(spark):
-    dep1DF = spark.read.format("jdbc").option("url", "jdbc:oracle:thin:@localhost:1521:orcl").option("driver","oracle.jdbc.driver.OracleDriver").option("dbtable", "COLLECTOR.DEPARTMENT1").option("user", "pyspark").option("password", "pyspark").load()
-    custDF = spark.read.format("jdbc").option("url", "jdbc:oracle:thin:@localhost:1521:orcl").option("driver","oracle.jdbc.driver.OracleDriver").option("dbtable", "COLLECTOR.CUSTOMERS").option("user", "pyspark").option("password", "pyspark").load()
-    departmentDF = custDF.subtract(dep1DF)
+    adult1DF = spark.read.format("jdbc").option("url", "jdbc:oracle:thin:@localhost:1521:orcl").option("driver","oracle.jdbc.driver.OracleDriver").option("dbtable", "COLLECTOR.ADULTS").option("user", "pyspark").option("password", "pyspark").load()
+    adult2DF = spark.read.format("jdbc").option("url", "jdbc:oracle:thin:@localhost:1521:orcl").option("driver","oracle.jdbc.driver.OracleDriver").option("dbtable", "COLLECTOR.ADULTS2").option("user", "pyspark").option("password", "pyspark").load()
+    writeDF = adult1DF.subtract(adult2DF)
     
 if __name__ == "__main__":
     spark = SparkSession.builder.appName("Python Spark SQL data source example").getOrCreate()
