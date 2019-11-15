5a0dbb52cbab41a54b0d5f081dac392108e15cf3
MageshAnand <magesh.anand6@gmail.com>
2019-11-15 12:43:20 +0530

    join example

--- oracle_jdbc_format.py
+++ oracle_jdbc_format.py
@@ -1,14 +1,13 @@
 from __future__ import print_function
-
 from pyspark.sql import SparkSession
 from pyspark.sql import Row
-
-def jdbc_dataset_example(spark):
-    df1 = spark.read.format("jdbc").option("url", "jdbc:oracle:thin:@localhost:1521:orcl").option("driver","oracle.jdbc.driver.OracleDriver").option("dbtable", "COLLECTOR.PEOPLE").option("user", "pyspark").option("password", "pyspark").load()
-    df2 = spark.read.format("jdbc").option("url", "jdbc:oracle:thin:@localhost:1521:orcl").option("driver","oracle.jdbc.driver.OracleDriver").option("dbtable", "COLLECTOR.ADULTS").option("user", "pyspark").option("password", "pyspark").load()
-    jdbcDF = df1.intersectAll(df2)
+ 
+def jdbc_join_example(spark):
+    dep1DF = spark.read.format("jdbc").option("url", "jdbc:oracle:thin:@localhost:1521:orcl").option("driver","oracle.jdbc.driver.OracleDriver").option("dbtable", "COLLECTOR.DEPARTMENT1").option("user", "pyspark").option("password", "pyspark").load()
+    dep2DF = spark.read.format("jdbc").option("url", "jdbc:oracle:thin:@localhost:1521:orcl").option("driver","oracle.jdbc.driver.OracleDriver").option("dbtable", "COLLECTOR.DEPARTMENT2").option("user", "pyspark").option("password", "pyspark").load()
+    departmentDF = dep1DF.join(dep2DF)
     
 if __name__ == "__main__":
     spark = SparkSession.builder.appName("Python Spark SQL data source example").getOrCreate()
-    jdbc_dataset_example(spark)
+    jdbc_join_example(spark)
     spark.stop()
