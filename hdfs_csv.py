8297bd119679a684b69884dc4486794521291bb5
MageshAnand <magesh.anand6@gmail.com>
2019-11-18 13:32:59 +0530

    Update hdfs_csv.py

--- hdfs_csv.py
+++ hdfs_csv.py
@@ -1,8 +1,8 @@
 from pyspark.sql import SparkSession
 
 def basic_datasource_example_na(spark):
-	df = spark.read.csv("hdfs://tmp/HDFS/country.csv")
-	df.write.csv("hdfs://tmp/HDFS/city.csv")
+	df = spark.read.csv("hdfs:///tmp/HDFS/country.csv")
+	df.write.csv("hdfs:///tmp/HDFS/city.csv")
 	
 if __name__ == "__main__":
     spark = SparkSession.builder.appName("Python Spark SQL data source example").getOrCreate()
