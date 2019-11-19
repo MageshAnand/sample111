fd19c7b52c5a53fc8657af8fdc16fe7566aa7b15
MageshAnand <magesh.anand6@gmail.com>
2019-11-19 11:50:17 +0530

    Update hdfs_read.py

--- hdfs_read.py
+++ hdfs_read.py
@@ -1,8 +1,8 @@
 from pyspark.sql import SparkSession
 
 def basic_read_write(spark):
-	df = spark.read.load("examples/src/main/resources/userdata.parquet")
-	df.write.save("namesAndFavColors.parquet")
+	df = spark.read.load("hdfs:///tmp/HDFS/userdata.parquet")
+	df.write.save("hdfs:///tmp/HDFS/namesAndFavColors.parquet")
 
 if __name__ == "__main__":
     spark = SparkSession.builder.appName("Python Spark SQL data source example").getOrCreate()
