f709316d958015a55a56aa2193cc365faf5ae9b5
MageshAnand <magesh.anand6@gmail.com>
2019-11-19 11:47:21 +0530

    read

---
+++ hdfs_read.py
@@ -0,0 +1,10 @@
+from pyspark.sql import SparkSession
+
+def basic_read_write(spark):
+	df = spark.read.load("examples/src/main/resources/userdata.parquet")
+	df.select("name", "favorite_color").write.save("namesAndFavColors.parquet")
+
+if __name__ == "__main__":
+    spark = SparkSession.builder.appName("Python Spark SQL data source example").getOrCreate()
+    basic_read_write(spark)
+    spark.stop()
