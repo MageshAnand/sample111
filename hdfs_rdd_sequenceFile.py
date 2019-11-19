ac20961d94df584a50ff448793efe8c25983ec30
MageshAnand <magesh.anand6@gmail.com>
2019-11-19 13:06:16 +0530

    hadoop sequence file

---
+++ hdfs_rdd_sequenceFile.py
@@ -0,0 +1,13 @@
+import sys
+
+from pyspark import SparkContext
+
+def rddHadoopSequence(sc):
+ rddData = sc.sequenceFile("hdfs:///tmp/HDFS/RDD/source, "org.apache.hadoop.io.Text", "org.apache.hadoop.io.DoubleWritable")
+ rddData.saveAsSequenceFile("hdfs:///tmp/HDFS/RDD/target")
+
+if __name__ == "__main__":
+ # create Spark context with Spark configuration
+ conf = SparkConf().setAppName("Read sequence to RDD - Python")
+ sc = SparkContext(conf)
+ rddHadoopSequence(sc)
